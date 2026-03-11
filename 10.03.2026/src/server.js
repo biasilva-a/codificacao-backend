ce5fed1f69748b6df75dba4449132394824d4769
Bia <bianca_og_silva@estudante.sesisenai.org.br>
2026-03-11 16:57:01 -0300

    Aula 11.03

--- 10.03.2026/src/server.js
+++ 10.03.2026/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const routes = require('./routes/routes')
 const app = express()
-const port = 3000
+app.use(cors())
+app.use(express.json())
+app.use('10.03.2026',routes)
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
