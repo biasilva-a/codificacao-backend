aed801710e4d6faf79af0e173cf66e30631ebe27
Bia <bianca_og_silva@estudante.sesisenai.org.br>
2026-03-17 16:07:19 -0300

    Atividade de criar server

---
+++ 17-03-2026/atv-criar-server/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.json('API de Teste rodando com sucesso!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
